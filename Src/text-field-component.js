a6811afcab1b4c68e924c8acc18ea4371d8e08a9
Pratham2604 <pratham@quantumgraph.com>
2016-07-31 10:43:04 +0530

    First commit: adding base code for input text field

---
+++ Src/text-field-component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class AdvancedTextField extends React.Component {
+  render() {
+    return (
+      <div> 
+        <input type="text"/>
+      </div>
+    )
+  }
+}
